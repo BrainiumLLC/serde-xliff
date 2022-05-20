cb2a40929c7e35bd5546e2bd590255ae4d1c11bd
Arthur Valladares <arthur@brainiumstudios.com>
2022-05-20 12:52:18 -0700

    Initial implementation of Deserializing xliff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use serde::{Deserialize, Serialize};
+use std::{fs::File as StdFile, io::BufReader};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct Xliff {
+    version: String,
+    xmln: String,
+    //#[serde(rename = "$value")]
+    file: File,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "kebab-case")]
+struct File {
+    original: String,
+    source_language: String,
+    target_language: String,
+    datatype: String,
+    body: Body,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "kebab-case")]
+struct Body {
+    trans_unit: Vec<TransUnit>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct TransUnit {
+    id: String,
+    source: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let f = StdFile::open("test.xliff").expect("Could not open file");
+    let mut reader = BufReader::new(f);
+    let xliff: Xliff =
+        serde_xml_rs::de::from_reader(reader).expect("Could not create Xliff object");
+    println!("{:#?}", xliff);
 }
