3fe77d005a0395f079b101f822e0f919607c2c9d
Arthur Valladares <arthur@brainiumstudios.com>
2022-05-31 11:07:38 -0700

    Start working on string with arguments

--- src/xliff.rs
+++ src/xliff.rs
@@ -1,19 +1,50 @@
-use serde::{Deserialize, Serialize};
+use serde::{de::Visitor, Deserialize, Deserializer};
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Debug)]
+pub struct ArgumentString {
+    // TODO: `&'static str`?
+    sections: Vec<String>,
+}
+
+impl From<String> for ArgumentString {
+    fn from(string: String) -> Self {
+        let sections = string.split("%").map(str::to_string).collect::<Vec<_>>();
+        Self { sections }
+    }
+}
+
+struct ArgumentStringVisitor;
+impl<'de> Visitor<'de> for ArgumentStringVisitor {
+    type Value = ArgumentString;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a string with optional arguments marked by `%`")
+    }
+}
+
+impl<'de> Deserialize<'de> for ArgumentString {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(ArgumentStringVisitor)
+    }
+}
+
+#[derive(Debug, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct TransUnit {
     pub id: String,
-    pub source: String,
+    pub source: ArgumentString,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Body {
     trans_unit: Vec<TransUnit>,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct File {
     original: String,
@@ -23,7 +54,7 @@ pub struct File {
     body: Body,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub struct Xliff {
     version: String,
